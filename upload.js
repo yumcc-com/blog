643ae1033d5f26e82b6d71a16d2a80fd41dda625
‘linshicheng <linshicheng@dyg.cn>
2021-07-13 16:07:50 +0800

    上传流程新增删除线上资源

--- upload.js
+++ upload.js
@@ -1,6 +1,14 @@
 const COS = require('cos-nodejs-sdk-v5');
 const Archiver = require('archiver');
-const { SecretId, SecretKey, Bucket, Region, FileKey, FilePath, DirPath } = require('./config.js');
+const {
+  SecretId,
+  SecretKey,
+  Bucket,
+  Region,
+  FileKey,
+  FilePath,
+  DirPath
+} = require('./config.js');
 const Fs = require('fs');
 
 (async () => {
@@ -30,15 +38,89 @@ const Fs = require('fs');
     })
   }
 
+  const deleteAll = (prefix) => {
+    return new Promise((res, rej) => {
+      const deleteFolder = (marker) => {
+        cos.getBucket({
+          Bucket,
+          Region,
+          Prefix: prefix,
+          Marker: marker,
+          MaxKeys: 1000,
+        }, function (listError, listResult) {
+          if (listError) {
+            console.log('list error:', listError);
+            rej(listError)
+          }
+          var nextMarker = listResult.NextMarker;
+          var objects = listResult.Contents.map(function (item) {
+            return {
+              Key: item.Key
+            }
+          });
+          cos.deleteMultipleObject({
+            Bucket,
+            Region,
+            Objects: objects,
+          }, function (delError, deleteResult) {
+            if (delError) {
+              console.log('delete error', delError);
+              console.log('delete stop');
+              rej(delError)
+            } else {
+              console.log('delete result', deleteResult);
+              if (listResult.IsTruncated === 'true') deleteFolder(nextMarker);
+              else res();
+            }
+          });
+        });
+      }
+
+      deleteFolder()
+    })
+  }
+
+  const getPrefix = () => {
+    return new Promise((res, rej) => {
+      cos.getBucket({
+        Bucket,
+        Region,
+        Delimiter: '/',
+      }, async function (err, data) {
+        if (data.CommonPrefixes) {
+          const arr = data.CommonPrefixes.map(item => item.Prefix)
+
+          function find(str) {
+            return str >= 'image/';
+          }
+          arr.splice(arr.findIndex(find), 1)
+          const promises = arr.map(item => deleteAll(item))
+          console.log(promises)
+          Promise.all(promises).then((result) => {
+            console.log(result) //['成功了', 'success']
+            res()
+          }).catch((error) => {
+            console.log(error)
+            rej(error)
+          })
+        }
+      })
+    })
+  }
+
   const output = Fs.createWriteStream(FilePath); // 压缩后的文件路径(和上传的文件路径一致)
   const archive = Archiver('zip', {
-    zlib: { level: 9 }
+    zlib: {
+      level: 9
+    }
   });
 
-  output.on('close', () => {
+  output.on('close', async () => {
     console.log(archive.pointer() + ' total bytes')
     console.log('Archiver has been finalized and the output file descriptor has closed.')
     console.log(`✔文件压缩完成~`)
+    console.log('开始删除线上资源')
+    await getPrefix()
     upload()
   });
 
@@ -54,11 +136,11 @@ const Fs = require('fs');
       throw err
     }
   });
-  
+
   archive.on('error', (err) => {
     throw err
   });
-  
+
   archive.pipe(output);
 
   archive.directory(DirPath, false);
