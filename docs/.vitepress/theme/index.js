36296ffb0d4d88068d3f93c87bb823f09e4531e1
林诗程 <linshicheng@dyg.cn>
2022-03-03 11:29:28 +0800

    feat：新增彩带特效

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,4 +1,8 @@
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
+import IndexLayout from './indexLayout.vue'
 
-export default DefaultTheme
+export default {
+  ...DefaultTheme,
+  Layout: IndexLayout
+}
---
+++ docs/.vitepress/theme/indexLayout.vue
@@ -0,0 +1,41 @@
+<template>
+  <Layout>
+    <template #home-hero></template>
+  </Layout>
+</template>
+
+<script setup>
+import confetti from 'canvas-confetti'
+import DefaultTheme from 'vitepress/theme'
+import { onMounted } from 'vue'
+const { Layout } = DefaultTheme
+
+const end = Date.now() + (1.2 * 1000)
+const colors = ['#6470ff', '#75ffc0']
+
+// 彩带特效
+const frame = () => {
+  confetti({
+    particleCount: 2,
+    angle: 60,
+    spread: 55,
+    origin: { x: 0 },
+    colors
+  })
+  confetti({
+    particleCount: 2,
+    angle: 120,
+    spread: 55,
+    origin: { x: 1 },
+    colors
+  })
+
+  if (Date.now() < end) {
+    requestAnimationFrame(frame)
+  }
+}
+
+onMounted(() => {
+  frame()
+})
+</script>
